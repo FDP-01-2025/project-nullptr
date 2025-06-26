6f65d3add26df7221661adb0b2105749e4141db7
FelipePalacios23 <00016325@uca.edu.sv>
2025-06-26 17:54:17 -0600

    MAIN and some hangman prep stuff

---
+++ src/hangman_fun.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include "hangman_fun.h"
+using namespace std;
+
+//defining functions here!!!
+
+void hangman_intro()
+{
+    cout<<"AHORCADO/n";
+    cout<<"-------------------/n";
+    cout<<"Objetivo: Salva a x de ser ahorcado, introduce la clave letra por letra./n";
+    cout<<"-------------------/n";
+}
