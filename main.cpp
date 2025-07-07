6563bad16a14a677501411327a12692363d61901
EduardoPacas25 <00134525@uca.edu.sv>
2025-07-07 17:47:11 -0600

    Save game system implemented

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 #include "src\stealmaze.cpp"
 #include "src\hiddentreasure.cpp"
 #include "src\hangman.cpp"
@@ -9,128 +10,146 @@
 #include "src\blackjack.cpp"
 #include "src\intromenu.h"
 using namespace std;
-int main(){
-int lifes=3;
-bool passed;  
-    //Intromenu
-    passed=intromenu();
-    if (!passed)
-    {
-        return 0;
+
+struct saveData {
+    int lives;
+    int currentStage;
+};
+
+void saveProgress(const saveData& data) {
+    ofstream file("gameSaveFile.txt");
+    if (file.is_open()) {
+        file << data.lives << endl;
+        file << data.currentStage << endl;
+        file.close();
     }
-    else
-    //Blackjack
-     while (lifes > 0) {
-        passed = playblackjack();  
+}
+
+bool loadProgress(saveData& data) {
+    ifstream file("gameSaveFile.txt");
+    if (file.is_open()) {
+        file >> data.lives;
+        file >> data.currentStage;
+        file.close();
+        return true;
+    }
+    return false;
+}
+
+void promptToSave(int lives, int stage) {
+    char saveOpt;
+    cout << "Do you want to save your progress? [Y/N]: ";
+    cin >> saveOpt;
+    if (saveOpt == 'Y' || saveOpt == 'y') {
+        saveProgress({lives, stage});
+        cout << "Progress saved.\n";
+    }
+}
+
+int main() {
+    saveData data = {3, 0};
+    char option;
+
+    cout << "Do you want to load a saved game? [Y]es  [N]o: ";
+    cin >> option;
+
+    if (option == 'Y' || option == 'y') {
+        if (loadProgress(data)) {
+            cout << "Game loaded.\n";
+        } else {
+            cout << "Save file not found. Starting new game.\n";
+        }
+    }
+
+    int lives = data.lives;
+    int stage = data.currentStage;
+    bool passed;
+
+    if (stage == 0) {
+        passed = intromenu();
+        if (!passed) return 0;
+        stage = 1;
+        promptToSave(lives, stage);
+    }
+
+    while (lives > 0 && stage == 1) {
+        passed = playblackjack();
         if (passed) {
+            stage = 2;
+            promptToSave(lives, stage);
             break;
         } else {
-            lifes--;
-            cout << "You got  " << lifes  << " tries left\n";
+            lives--;
+            cout << "You got " << lives << " tries left\n";
         }
     }
-        if (lifes == 0)
-    {
-        cout << "GAME OVER\n";
-        return 0;
-    }
-        //BANK MINIGAME?
-    while (lifes > 0) {
-        passed = alphnumbseq();  
+    if (lives == 0) return cout << "GAME OVER\n", 0;
+
+    while (lives > 0 && stage == 2) {
+        passed = alphnumbseq();
         if (passed) {
+            stage = 3;
+            promptToSave(lives, stage);
             break;
-        }
-        else
-        {
-            lifes--;
-            cout << "You got  " << lifes << " tries left\n";
+        } else {
+            lives--;
+            cout << "You got " << lives << " tries left\n";
         }
     }
+    if (lives == 0) return cout << "GAME OVER\n", 0;
 
-    if (lifes == 0)
-    {
-        cout << "GAME OVER\n";
-        return 0;
-    }
-    // MUSEUM STEAL
-    while (lifes > 0)
-    {
+    while (lives > 0 && stage == 3) {
         passed = playhiddentreasure();
-        if (passed)
-        {
+        if (passed) {
+            stage = 4;
+            promptToSave(lives, stage);
             break;
-        }
-        else
-        {
-            lifes--;
-            cout << "You got  " << lifes << " tries left\n";
+        } else {
+            lives--;
+            cout << "You got " << lives << " tries left\n";
         }
     }
+    if (lives == 0) return cout << "GAME OVER\n", 0;
 
-    if (lifes == 0)
-    {
-        cout << "GAME OVER\n";
-        return 0;
-    }
-
-    while (lifes > 0)
-    {
+    while (lives > 0 && stage == 4) {
         passed = playstealmaze();
-        if (passed)
-        {
+        if (passed) {
+            stage = 5;
+            promptToSave(lives, stage);
             break;
-        }
-        else
-        {
-            lifes--;
-            cout << "You got  " << lifes << " tries left\n";
+        } else {
+            lives--;
+            cout << "You got " << lives << " tries left\n";
         }
     }
+    if (lives == 0) return cout << "GAME OVER\n", 0;
 
-    if (lifes == 0)
-    {
-        cout << "GAME OVER\n";
-        return 0;
-    }
-    // BANKS STEAL
-    while (lifes > 0)
-    {
+    while (lives > 0 && stage == 5) {
         passed = playhangman();
-        if (passed)
-        {
+        if (passed) {
+            stage = 6;
+            promptToSave(lives, stage);
             break;
-        }
-        else
-        {
-            lifes--;
-            cout << "You got  " << lifes << " tries left\n";
+        } else {
+            lives--;
+            cout << "You got " << lives << " tries left\n";
         }
     }
+    if (lives == 0) return cout << "GAME OVER\n", 0;
 
-    if (lifes == 0)
-    {
-        cout << "GAME OVER\n";
-        return 0;
-    }
-    // MINESWEPPER
-    while (lifes > 0)
-    {
+    while (lives > 0 && stage == 6) {
         passed = playminesweeper();
-        if (passed)
-        {
+        if (passed) {
+            stage = 7;
+            cout << "Congratulations, you completed the game!!!\n";
+            remove("gameSaveFile.txt");
             break;
-        }
-        else
-        {
-            lifes--;
-            cout << "You got  " << lifes << " tries left\n";
+        } else {
+            lives--;
+            cout << "You got " << lives << " tries left\n";
         }
     }
+    if (lives == 0) return cout << "GAME OVER\n", 0;
 
-    if (lifes == 0)
-    {
-        cout << " GAME OVER\n";
-        return 0;
-    }
-    cout << "The bool works";
+    return 0;
 }
