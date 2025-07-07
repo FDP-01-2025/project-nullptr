59335d11d9d84fb0d500ab82e9256285734d7349
00157825 <00157825@uca.edu.sv>
2025-07-07 17:33:16 -0600

    Changes on minesweeper

--- src/minesweepergame.h
+++ src/minesweepergame.h
@@ -7,15 +7,15 @@ using namespace std;
 
 struct Cell // Structure to represent each cell
 {
-   bool hasMine; //True if the cell contains a mine
+   bool hasSensor; //True if the cell contains a mine
    bool revealed;//True if the player reveals the cell
-   bool hasFlag; //True if the player puts a flag on a cell
-   int MinesNear; //Number of mines around the selected cell
+   bool hasShield; //True if the player puts a flag on a cell
+   int SensorsNear; //Number of mines around the selected cell
 };
 
 //Global variables (can be used on all functions)
 extern int SIZE; //Board size (6x6)
-extern int Mines_Number; //Number of mines around the board
+extern int Sensors_Number; //Number of mines around the board
 extern Cell grid[6][6]; // Game board (array for 36 cells)
 extern bool GameOver; // True when the game ends(win or lose)
 extern bool Victory;//True when the player wins
